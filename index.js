584f5bc1d426a04d3f87b5585a823b729a02631f
Tehreemaarif12 <handmadesparkless.@gmail.com>
2025-04-06 23:56:08 +0500

    Add HTML, CSS, and JS files

---
+++ index.js
@@ -0,0 +1,13 @@
+function calculateSum() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const sum = num1 + num2;
+    document.getElementById('result').textContent = `The sum is: ${sum}`;
+  }
+  
+  document.getElementById('calcButton').addEventListener('click', calculateSum);
+  
+
+  
+
+  
